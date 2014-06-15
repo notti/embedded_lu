e0b0588819ba5e8b3abe1586359a8f389fb40dab
Gernot Vormayr <gvormayr@gmail.com>
2014-06-15 18:31:47 +0200

    static version of modules

--- lab1/task2/lfsr.h
+++ lab1/task2/lfsr.h
@@ -12,8 +12,8 @@ SC_MODULE(lfsr) {
     sc_inout<sc_bv<8> > out;
 
 
-    shiftreg *sr;
-    lfsr_feedback *fb;
+    shiftreg sr;
+    lfsr_feedback fb;
 
     sc_signal<bool> rightin;
 
@@ -21,23 +21,21 @@ SC_MODULE(lfsr) {
     sc_signal<sc_bv<8> > in;
     sc_signal<bool> zero;
 
-    SC_CTOR(lfsr) {
+    SC_CTOR(lfsr) : sr("shiftreg"), fb("feedback") {
         zero.write(0);
 
-        sr = new shiftreg("shiftreg");
-        sr->clk(clk); 
-        sr->reset(reset);
-        sr->ld(zero);
-        sr->lshift(count_en);
-        sr->rshift(zero);
-        sr->leftin(zero);
-        sr->in(in);
-        sr->rightin(rightin);
-        sr->out(out);
+        sr.clk(clk); 
+        sr.reset(reset);
+        sr.ld(zero);
+        sr.lshift(count_en);
+        sr.rshift(zero);
+        sr.leftin(zero);
+        sr.in(in);
+        sr.rightin(rightin);
+        sr.out(out);
 
-        fb = new lfsr_feedback("feedback");
-        fb->in(out);
-        fb->out(rightin);
+        fb.in(out);
+        fb.out(rightin);
     }   
 
 
